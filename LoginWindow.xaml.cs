a53b433b3af5c3c29dcac78b1ecf532f7b311981
Marek <wojnarmarek0@gmail.com>
2022-06-06 21:21:26 +0200

    Add project files.

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace Desktop_App
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+        public void handleLoginUser(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-DG8OM09\SQLEXPRESS;Initial Catalog=medicine_base;Integrated Security=True");
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                String query = "SELECT COUNT(1) FROM User_Table WHERE user_name = @UserName AND user_password=@UserPassword";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = System.Data.CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", UserLogin.Text);
+                sqlCmd.Parameters.AddWithValue("@UserPassword", UserPassword.Text);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                } else
+                {
+                    MessageBox.Show("User Invalid!");
+                }
+
+            } catch(Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            } finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+    }
+}
